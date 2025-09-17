7b589e01aaa8ce0991fb33ed649519dcae554db1
JonathanMaesen <tiwaztyr1802@gmail.com>
2025-09-17 13:15:06 +0200

    Regenboog done

--- labo-1-Regenboog/src/App.tsx
+++ labo-1-Regenboog/src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css'
 
+
 function App() {
 
-  return (
+    const colors = Array.from({length: 100}, (_, i) => `hsl(${i * 360 / 100}, 100%, 50%)`);
+
+    return (
     <>
+        <div style={{height: "100vh", width: "100%"}}>
+            {colors.map((color, i) => (
+                <div
+                    key={i}
+                    style={{
+                        width: "100%",
+                        height: "8px",
+                        backgroundColor: color,
+                        display: 'block'
+                }}
+                ></div>
+            ))}
+        </div>
     </>
-  )
+  );
 }
 
 export default App
