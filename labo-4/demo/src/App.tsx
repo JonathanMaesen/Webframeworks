0bee775f247d3fa8e2338dd7785e383caafac558
Jonathan <tiwaztyr1802@gmail.com>
2025-09-22 11:44:42 +0200

    lessen done

--- labo-4/demo/src/App.tsx
+++ labo-4/demo/src/App.tsx
@@ -1,9 +1,12 @@
+// @ts-ignore
 import './App.css';
+import {useState} from "react";
 
 function App() {
-
+    const [name, setname] = useState<string>("");
     return (
         <>
+
         </>
     );
 }
