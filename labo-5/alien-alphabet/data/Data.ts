715bd51e8fb29856b9db042487da8b4a299d9638
Jonathan <tiwaztyr1802@gmail.com>
2025-10-09 13:40:34 +0200

    Alien-Alphabet done

---
+++ labo-5/alien-alphabet/data/Data.ts
@@ -0,0 +1,15 @@
+export async function loadJson<T = unknown>(url: string): Promise<T> {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`Failed to load JSON: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    if (data === null || typeof data !== "object" || Array.isArray(data)) {
+        throw new Error("Expected a single JSON object.");
+    }
+
+    return data as T;
+}
---
+++ labo-5/alien-alphabet/src/App.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { loadJson } from "../data/Data";
+
+interface letter {
+    human: string;
+    alien: string
+}
+
+interface AlienAlphabet {
+    letters: letter[];
+}
+
+export default function App() {
+    const [alphabet, setAlphabet] = useState<AlienAlphabet>();
+    const [chosenLetters, setChosenLetters] = useState<letter[]>([]);
+    useEffect(() => {
+        loadJson<AlienAlphabet>(
+            "https://raw.githubusercontent.com/slimmii/alien-alphabet/master/alien.json"
+        )
+            .then(setAlphabet)
+    }, []);
+
+    if (!alphabet) return <div>Loading…</div>;
+
+    const AddLetter = (item: letter) => {
+      setChosenLetters((prevState) => [...prevState, item])
+    }
+    const Clear = () => {
+      setChosenLetters([])
+    }
+    const Backspace = () => {
+        setChosenLetters(prev => prev.slice(0, -1));
+    }
+
+    return (
+        <>
+            <div>
+                <h3>Controls</h3>
+                <button onClick={Clear}>Clear</button>
+                <button onClick={Backspace}>Backspace</button>
+            </div>
+            <div>
+                {alphabet.letters.map((letter, i) => (
+                    <button key={i} onClick={() => AddLetter(letter)}>
+                        <img src={letter.alien} alt={letter.human} /> = {letter.human}
+                    </button>
+                ))}
+            </div>
+            <div>
+                {chosenLetters.map((letter, i) => (
+                    <button key={i}> <img src={letter.alien} alt={letter.human} /> = {letter.human}
+                    </button>
+                ))}
+            </div>
+        </>
+    );
+}
