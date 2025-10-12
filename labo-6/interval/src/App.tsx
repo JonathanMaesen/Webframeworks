74ca721f903c8c362d46ddb07f8f4e2ac8cebd16
JonathanMaesen <tiwaztyr1802@gmail.com>
2025-10-12 21:03:13 +0200

    coole dingen

--- labo-6/interval/src/App.tsx
+++ labo-6/interval/src/App.tsx
@@ -1,8 +1,34 @@
-import './App.css';
+import { useEffect,useState } from "react";
 
-function App() {
+interface TimerProps {
+    interval: number
+}
+
+const Timer = ({interval} : TimerProps) => {
+    const [number, setNumber] = useState(0);
+    useEffect(() => {
+        let handle = setInterval(() => {
+            setNumber(number => number + 1);
+        },interval);
+
+        return () => {
+            clearInterval(handle);
+        }
+    },[interval]);
+
+    return <p>{number}</p>
+}
+
+
+const App = () => {
+    const [timerInterval, setTimerInterval] = useState(1000);
     return (
         <>
+            <div>
+                <input type="range" min="1" max="2000" value={timerInterval} onChange={(e) => setTimerInterval(parseInt(e.target.value))}/>
+                <label htmlFor="volume">{timerInterval}</label>
+            </div>
+            <Timer interval={timerInterval} />
         </>
     );
 }
