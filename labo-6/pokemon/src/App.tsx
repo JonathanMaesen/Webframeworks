3499547e77f2d624459fc420bc1384996799fdfb
JonathanMaesen <tiwaztyr1802@gmail.com>
2025-10-12 21:02:52 +0200

    fixed pokemon to full spec

--- labo-6/pokemon/src/App.tsx
+++ labo-6/pokemon/src/App.tsx
@@ -1,47 +1,62 @@
 import './App.css';
-import {useEffect, useState} from "react";
-interface PokemonList {
-    id: number
-    name: string
+import { useEffect, useState } from "react";
+
+interface Pokemon {
+    name: string;
+    url: string;
 }
-function Pokedex(limit: number) {
+
+function Pokedex() {
     const [limit, setLimit] = useState<number>(0);
-    const [pokemonList, setPokemonList] = useState<PokemonList[]>([])
-    useEffect(( ) => {
+    const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+
+    useEffect(() => {
         let ignore = false;
-        const fetchFunction = async() => {
-            let result = await fetch(`https://pokeapi.co/api/v2/ability/?limit=${limit}`);
-            let json = await result.json();
-            if (!ignore) {
-                setPokemonList(json);
+
+        const fetchFunction = async () => {
+            const result = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}`);
+            const json = await result.json();
+
+            if (!ignore && json.results) {
+                setPokemonList(json.results);
             }
-        }
-        fetchFunction();
+        };
+
+        if (limit > 0) fetchFunction();
+
         return () => {
             ignore = true;
-        }
-    }, [])
-    return(
-        <>
-            {pokemonList.map((item) => (
-                <div key={item.id}>
-                    {item.name}
-                </div>
-            ))}
-            <input
-                type="number"
-                value={limit}
-                onChange={(e) => setLimit(Number(e.target.value))}
-            />
-        </>
-    )
-}
-function App() {
+        };
+    }, [limit]);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        const input = form.elements.namedItem("limitInput") as HTMLInputElement;
+        const newLimit = Number(input.value);
+        setLimit(newLimit);
+    };
 
     return (
         <>
+            {pokemonList.map((item, index) => (
+                <div key={index}>{item.name}</div>
+            ))}
+
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="number"
+                        name="limitInput"
+                        placeholder="Number of Pokémon"
+                    />
+                    <button type="submit">Fetch</button>
+                </form>
         </>
     );
+}
+
+function App() {
+    return <Pokedex />;
 }
 
 export default App;
