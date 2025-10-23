674e48362c2e284921bc5ff1c20f64fb011a87f5
Jonathan <tiwaztyr1802@gmail.com>
2025-10-23 14:11:18 +0200

    les begint

--- JsReact/labo-8/settings/src/App.tsx
+++ JsReact/labo-8/settings/src/App.tsx
@@ -1,5 +1,16 @@
 import './App.css';
+import {createContext} from "react";
 
+interface SettingsContext {
+    color: string
+}
+
+const SettingsContext = createContext<SettingsContext>({color: "blue"})
+
+function Square() {
+
+    
+}
 function App() {
     return (
         <>
